81fe0f967cdb83d54119ab30cf07384e3341a16e
fengyuanliang3 <fengyuanliang2@jd.com>
2019-03-18 23:13:41 +0800

    feat: block颜色兼容性

--- lib/components/NumberText.dart
+++ lib/components/NumberText.dart
@@ -1,50 +1,36 @@
 import 'package:flutter/material.dart';
 
-var styles = {
-  2: {
-    "color": Color(0xff776e65),
-    "background": Color(0xffeee4da),
-  },
-  4: {
-    "color": Color(0xff776e65),
-    "background": Color(0xffede0c8),
-  },
-  8: {
-    "color": Color(0xfff9f6f2),
-    "background": Color(0xfff2b179),
-  },
-  16: {
-    "color": Color(0xfff9f6f2),
-    "background": Color(0xfff59563),
-  },
-  32: {
-    "color": Color(0xfff9f6f2),
-    "background": Color(0xfff67c5f),
-  },
-  64: {
-    "color": Color(0xfff9f6f2),
-    "background": Color(0xfff65e3b),
-  },
-  128: {
-    "color": Color(0xfff9f6f2),
-    "background": Color(0xffedcf72),
-  },
-  256: {
-    "color": Color(0xfff9f6f2),
-    "background": Color(0xfff9f6f2),
-  },
-  512: {
-    "color": Color(0xfff9f6f2),
-    "background": Color(0xffedc850),
-  },
-  1024: {
-    "background": Color(0xffedc53f),
-  },
-  2048: {
-    "color": Color(0xfff9f6f2),
-    "background": Color(0xffedc22e),
-  }
-};
+class BlockStyle {
+  Color textColor;
+  Color background;
+
+  BlockStyle({this.textColor, this.background});
+}
+
+var styles = Map.fromEntries([
+  MapEntry(2,
+      BlockStyle(textColor: Color(0xff776e65), background: Color(0xffeee4da))),
+  MapEntry(4,
+      BlockStyle(textColor: Color(0xff776e65), background: Color(0xffede0c8))),
+  MapEntry(8,
+      BlockStyle(textColor: Color(0xfff9f6f2), background: Color(0xfff2b179))),
+  MapEntry(16,
+      BlockStyle(textColor: Color(0xfff9f6f2), background: Color(0xfff59563))),
+  MapEntry(32,
+      BlockStyle(textColor: Color(0xfff9f6f2), background: Color(0xfff67c5f))),
+  MapEntry(64,
+      BlockStyle(textColor: Color(0xfff9f6f2), background: Color(0xfff65e3b))),
+  MapEntry(128,
+      BlockStyle(textColor: Color(0xfff9f6f2), background: Color(0xffedcf72))),
+  MapEntry(256,
+      BlockStyle(textColor: Color(0xfff9f6f2), background: Color(0xfff9f6f2))),
+  MapEntry(512,
+      BlockStyle(textColor: Color(0xfff9f6f2), background: Color(0xffedc850))),
+  MapEntry(1024,
+      BlockStyle(textColor: Color(0xfff9f6f2), background: Color(0xffedc53f))),
+  MapEntry(2048,
+      BlockStyle(textColor: Color(0xfff9f6f2), background: Color(0xffedc22e))),
+]);
 
 class NumberText extends StatelessWidget {
   final int value;
@@ -59,7 +45,9 @@ class NumberText extends StatelessWidget {
       width: size,
       height: size,
       decoration: BoxDecoration(
-        color: styles[this.value]['background'],
+        color: (this.value ~/ 2048) > 0
+            ? styles[this.value ~/ 2048].background
+            : styles[this.value].background,
         border: Border.all(color: Colors.transparent, width: 0),
         borderRadius: BorderRadius.circular(5),
       ),
@@ -67,7 +55,9 @@ class NumberText extends StatelessWidget {
         child: Text(
           numberText,
           style: TextStyle(
-            color: styles[this.value]['color'],
+            color: (this.value ~/ 2048) > 0
+                ? styles[this.value ~/ 2048].textColor
+                : styles[this.value].textColor,
             fontSize: size / (numberText.length < 2 ? 2 : numberText.length),
           ),
         ),
